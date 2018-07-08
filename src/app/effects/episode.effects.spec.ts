958d83d7894fa69eff68f51705d8c7a5c0f9d13c
Sandi Barr <sandi.barr@prx.org>
2018-07-08 10:49:53 -0500

    fix specs

--- src/app/effects/episode.effects.spec.ts
+++ src/app/effects/episode.effects.spec.ts
@@ -1,8 +1,11 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Observable } from 'rxjs';
 
 import { EpisodeEffects } from './episode.effects';
+import { FeederService } from '../podcast/feeder.service';
 
 describe('EpisodeService', () => {
   let actions$: Observable<any>;
@@ -10,10 +13,8 @@ describe('EpisodeService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        EpisodeEffects,
-        provideMockActions(() => actions$)
-      ]
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers: [EpisodeEffects, FeederService, provideMockActions(() => actions$)]
     });
 
     effects = TestBed.get(EpisodeEffects);
--- src/app/effects/podcast.effects.spec.ts
+++ src/app/effects/podcast.effects.spec.ts
@@ -1,8 +1,11 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Observable } from 'rxjs';
 
 import { PodcastEffects } from './podcast.effects';
+import { FeederService } from '../podcast/feeder.service';
 
 describe('PodcastService', () => {
   let actions$: Observable<any>;
@@ -10,10 +13,8 @@ describe('PodcastService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        PodcastEffects,
-        provideMockActions(() => actions$)
-      ]
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers: [PodcastEffects, FeederService, provideMockActions(() => actions$)]
     });
 
     effects = TestBed.get(PodcastEffects);
